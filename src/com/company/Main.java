39a3e5dc1dc0dbdc124514191a041e91bae7ae29
student <hhaskovic1@etf.unsa.ba>
2018-10-18 10:20:28 +0200

    Third commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,49 +1,49 @@
 package com.company;
 
-public class Main {
+
 
     class Sat {
-        private int sati;
-        private int  minute;
-        private int sekunde;
+    private int sati;
+    private int  minute;
+    private int sekunde;
 
 
-        Sat(int sati, int minute, int sekunde) {
-            postavi(sati,minute,sekunde);
-        }
-        private void postavi(int sati, int minute, int sekunde) {
-            this.sati=sati;
-            this.minute=minute;
-            this.sekunde=sekunde;
-        }
-        private void sljedeci() {
-            sekunde++;
-            if (sekunde==60) { sekunde=0; minute++; }
-            if (minute==60) { minute=0; sati++; }
-            if (sati==24) sati=0;
-        }
+    Sat(int sati, int minute, int sekunde) {
+        postavi(sati,minute,sekunde);
+    }
+        public void postavi(int sati, int minute, int sekunde) {
+        this.sati=sati;
+        this.minute=minute;
+        this.sekunde=sekunde;
+    }
+        public void sljedeci() {
+        sekunde++;
+        if (sekunde==60) { sekunde=0; minute++; }
+        if (minute==60) { minute=0; sati++; }
+        if (sati==24) sati=0;
+    }
 
-        private void prethodni() {
-            sekunde--;
-            if (sekunde==-1) { sekunde=59; minute--; }
-            if (minute==-1) { minute=59; sati--; }
-            if (sati==-1) sati=23;
-        }
-        private void pomjeriZa(int pomak) {
+        public void prethodni() {
+        sekunde--;
+        if (sekunde==-1) { sekunde=59; minute--; }
+        if (minute==-1) { minute=59; sati--; }
+        if (sati==-1) sati=23;
+    }
+        public void pomjeriZa(int pomak) {
             if (pomak>0)
                 for (int i=0; i<pomak; i++) sljedeci();
-        else
-            for (int i=0; i<-pomak; i++) prethodni();
+            else
+                for (int i=0; i<-pomak; i++) prethodni();
         }
-        private int dajSate() const { return sati; }
-        private int dajMinute() const { return minute; }
-        private int dajSekunde() const { return sekunde; }
-        private void ispisi() const { System.out.println( sati + minute + sekunde); }
+        public int getSati()  { return sati; }
+        public int getMinute()  { return minute; }
+        public int getSekunde() { return sekunde; }
+        public void ispisi() { System.out.println( sati + ":" + minute + ":" + sekunde); }
     };
 
-
+public class Main {
         public static void main(String[] args) {
-	Sat s(15,30,45);
+	Sat s = new Sat (15,30,45);
             s.ispisi();
             s.sljedeci();
             s.ispisi();
